7d499f58970396928312a25f5ab5eb53f4b7bf1b
ahmadbariish <ahmad.bariish@gmail.com>
2024-09-23 13:39:27 +0300

    end hearo

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,135 +1,54 @@
 import React from 'react';
-import logo from '../../assets/logo.svg';
-import './home.css';
-import { useState } from 'react';
-import {
-  Dialog,
-  DialogPanel,
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-  Popover,
-  PopoverButton,
-  PopoverGroup,
-  PopoverPanel,
-} from '@headlessui/react';
-import {
-  ArrowPathIcon,
-  Bars3Icon,
-  ChartPieIcon,
-  CursorArrowRaysIcon,
-  FingerPrintIcon,
-  SquaresPlusIcon,
-  XMarkIcon,
-} from '@heroicons/react/24/outline';
-
+import Header from './Header';
 const Home = () => {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   return (
-    <header className="bg-white fixed top-0 left-0 w-full z-50">
-      <nav
-        aria-label="Global"
-        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
-      >
-        <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5">
-            <span className="sr-only">Your Profile</span>
-            <img alt="" src={logo} className="w-12" />
-          </a>
-        </div>
-        <div className="flex lg:hidden">
-          <button
-            type="button"
-            onClick={() => setMobileMenuOpen(true)}
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-          >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon aria-hidden="true" className="h-6 w-6" />
-          </button>
-        </div>
-        <PopoverGroup className="hidden lg:flex lg:gap-x-12">
-          <a
-            href="#contact"
-            className="text-sm font-semibold leading-6 text-gray-900"
-          >
-            Contact
-          </a>
-          <a
-            href="#services"
-            className="text-sm font-semibold leading-6 text-gray-900"
-          >
-            Services
-          </a>
-          <a
-            href="#blog"
-            className="text-sm font-semibold leading-6 text-gray-900"
-          >
-            Posts
-          </a>
-        </PopoverGroup>
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            <span aria-hidden="true">&rarr;</span>
-          </a>
-        </div>
-      </nav>
-      <Dialog
-        open={mobileMenuOpen}
-        onClose={setMobileMenuOpen}
-        className="lg:hidden"
-      >
-        <div className="fixed inset-0 z-10" />
-        {/* screen size small */}
-        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Portfile</span>
-              <img alt="" src={logo} className="w-12" />
-            </a>
-            <button
-              type="button"
-              onClick={() => setMobileMenuOpen(false)}
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
-            >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon aria-hidden="true" className="h-6 w-6" />
-            </button>
-          </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                <a
-                  href="#contact"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Contact
-                </a>
-                <a
-                  href="#services"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Services
-                </a>
-                <a
-                  href="#blog"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Posts
-                </a>
-              </div>
-              <div className="py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Log in
-                </a>
-              </div>
+    <>
+      <Header />
+      <section className="pt-12 mt-5 bg-gray-50 sm:pt-16">
+        <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="text-center">
+            <p className="mt-2 max-w-4xl mx-auto mb-4 text-3xl font-bold leading-tight text-gray-900 sm:leading-tight sm:text-5xl lg:text-4xl lg:leading-tight">
+              Developing Innovative Web Solutions Using Artificial Intelligence
+            </p>
+            <h1 className="max-w-2xl mx-auto px-6 text-lg text-gray-600 font-inter">
+              With extensive experience in web development, I deliver customized
+              solutions tailored to your business needs. My services include
+              creating responsive, user-friendly websites and innovative
+              AI-driven tools to enhance customer engagement.
+            </h1>
+            <div className="px-8 sm:items-start sm:justify-center sm:px-0 sm:space-x-5 sm:flex mt-9">
+              <a
+                href="#contact"
+                title=""
+                className="mb-3 sm:mb-0 inline-flex items-center justify-center w-full px-8 py-3 text-lg font-bold text-white transition-all duration-200 bg-gray-900 border-2 border-transparent sm:w-auto rounded-xl hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+                role="button"
+              >
+                Request Service Now
+              </a>
+              <a
+                href="https://wa.me/4915211301829?text=Hello%20I%20need%20your%20services%20as%20a%20programmer."
+                className="inline-flex items-center justify-center w-full px-8 py-3 text-lg font-bold text-white transition-all duration-200 bg-green-500 border-2 border-transparent sm:w-auto rounded-xl hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-400"
+                role="button"
+                target="_blank"
+              >
+                Contact via WhatsApp
+              </a>
             </div>
           </div>
-        </DialogPanel>
-      </Dialog>
-    </header>
+        </div>
+        <div className="bg-gray-50 flex justify-center items-center mt-8">
+          <div className="relative flex justify-center items-center">
+            <div className="lg:max-w-4xl lg:mx-auto">
+              <img
+                className="px-0 "
+                src="https://images.unsplash.com/photo-1628277613967-6abca504d0ac"
+                alt="AI Chatbot"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
